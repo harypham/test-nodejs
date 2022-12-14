0e6213529031b532477804da66069ee63feffba8
Pham Tuan <tuan.pv@ptf.com.vn>
2022-12-14 15:26:58 +0700

    init project

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "db.oplxlbwqyuiorfkaospk.supabase.co",
+  USER: "postgres",
+  PASSWORD: "ktb0368435252aA@",
+  DB: "postgres",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
